dec6b3fc81505c61fa4323e526d4b98f81c5bf62
camizqu <62149994+camizqu@users.noreply.github.com>
2021-02-06 20:45:20 -0500

    seekbars and switch

--- app/src/main/java/com/example/parkingspace/MapsActivity.java
+++ app/src/main/java/com/example/parkingspace/MapsActivity.java
@@ -3,6 +3,10 @@ package com.example.parkingspace;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,21 +15,74 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    SeekBar seekBar;
+    SeekBar seekBar3;
+    Switch  switch2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapView5);
         mapFragment.getMapAsync(this);
-    }
 
-    /**
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        // perform seek bar change listener event used for getting the progress value
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MapsActivity.this, "Seek bar progress is :" + progressChangedValue,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        seekBar3 = (SeekBar) findViewById(R.id.seekBar3);
+        // perform seek bar change listener event used for getting the progress value
+        seekBar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar3, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar3) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar3) {
+                Toast.makeText(MapsActivity.this, "Seek bar progress is :" + progressChangedValue,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        switch2 = (Switch) findViewById(R.id.switch2);
+       // @Override
+                public void onClick(View view) {
+                    String statusSwitch2;
+                    if (switch2.isChecked())
+                        statusSwitch2 = switch2.getTextOn().toString();
+                    else
+                        statusSwitch2 = switch2.getTextOff().toString();
+                    Toast.makeText(getApplicationContext(),  "Switch2 :" + statusSwitch2, Toast.LENGTH_LONG).show(); // display the current state for switch's
+                });
+
+
+        /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
@@ -34,6 +91,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -43,4 +101,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-}
+
+
+}}
